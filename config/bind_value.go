35f0af7618adb96477e45f76fb1d16223abe620a
anhnmt <anhng.dev@gmail.com>
2025-03-15 13:43:56 +0700

    Optimize processField

--- config/bind_value.go
+++ config/bind_value.go
@@ -1,57 +1,47 @@
 package config
 
 import (
-    "reflect"
-    "strings"
+	"reflect"
+	"strings"
 
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
 )
 
 func unwrapPointer(iface any) (reflect.Value, reflect.Type) {
-    v := reflect.ValueOf(iface)
-    t := reflect.TypeOf(iface)
+	v := reflect.ValueOf(iface)
+	t := reflect.TypeOf(iface)
 
-    for v.Kind() == reflect.Pointer {
-        v = v.Elem()
-        t = t.Elem()
-    }
+	for v.Kind() == reflect.Pointer {
+		v = v.Elem()
+		t = t.Elem()
+	}
 
-    return v, t
+	return v, t
 }
 
 func bindValues(iface any, parts ...string) {
-    ifv, ift := unwrapPointer(iface)
-    processField(ifv, ift, parts)
+	ifv, ift := unwrapPointer(iface)
+	processField(ifv, ift, parts)
 }
 
 func processField(v reflect.Value, t reflect.Type, parts []string) {
-    for i := 0; i < t.NumField(); i++ {
-        field := t.Field(i)
-        fieldVal := v.Field(i)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fieldVal := v.Field(i)
 
-        tag, ok := field.Tag.Lookup("mapstructure")
-        if !ok {
-            continue
-        }
+		if tag, ok := field.Tag.Lookup("mapstructure"); ok {
+			key := strings.Join(append(parts, tag), ".")
+			envKey := strings.ToUpper(strings.ReplaceAll(key, ".", "_"))
 
-        var builder strings.Builder
-        if len(parts) > 0 {
-            builder.WriteString(strings.Join(parts, "."))
-            builder.WriteString(".")
-        }
-        builder.WriteString(tag)
+			_ = viper.BindEnv(key, envKey)
 
-        key := builder.String()
-        envKey := strings.ToUpper(strings.ReplaceAll(key, ".", "_"))
+			if value, hasDefault := field.Tag.Lookup("default"); hasDefault {
+				viper.SetDefault(key, value)
+			}
 
-        _ = viper.BindEnv(key, envKey)
-
-        if value, hasDefault := field.Tag.Lookup("defaultvalue"); hasDefault {
-            viper.SetDefault(key, value)
-        }
-
-        if fieldVal.Kind() == reflect.Struct {
-            processField(fieldVal, field.Type, append(parts, tag))
-        }
-    }
+			if fieldVal.Kind() == reflect.Struct {
+				processField(fieldVal, field.Type, append(parts, tag))
+			}
+		}
+	}
 }
